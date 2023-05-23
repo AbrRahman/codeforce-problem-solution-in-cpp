66e79196fe9f6577afc40cddd21fd1f4507292b5
AbrRahman <abrahman.est@gamil.com>
2023-05-23 22:30:12 +0600

    array max

---
+++ E_Max.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+      cin>>arr[i];
+    }
+    int Max = INT_MIN;
+    for(int i=0;i<n;i++){
+        Max = max(Max,arr[i]);
+    }
+    cout<<Max<<endl;
+    return 0;
+}
