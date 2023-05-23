4d6cdf704742d6829d440efce4043e5e682604ad
AbrRahman <abrahman.est@gamil.com>
2023-05-23 22:21:35 +0600

    array swap

---
+++ C_Simple_Calculator.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long long int x,y;
+    cin>>x>>y;
+    long long int sum = x+y;
+    long long int multi = x*y;
+    long long int sub=x-y;
+    cout<<x<<" + "<<y<<" = "<<sum<<endl;
+    cout<<x<<" * "<<y<<" = "<<multi<<endl;
+    cout<<x<<" - "<<y<<" = "<<sub;
+    return 0;
+}
