0e7b7d63aa1b84277a14384247dc162af5aa9f36
AbrRahman <abrahman.est@gamil.com>
2023-05-25 02:22:22 +0600

    aray palindrome

---
+++ G_Palindrome_Array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int i=0,j=n-1;
+    int flag =1;
+    while (i<j)
+    {
+       if(arr[i]!=arr[j]){
+          flag=0;
+       }
+       i++;
+       j--;
+    }
+    if(flag==1){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+    return 0;
+}
